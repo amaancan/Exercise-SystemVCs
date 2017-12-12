e1a55d281e958ca8a3ba256c165e864214e53a83
Amaan Khan <amaancan@gmail.com>
2017-12-12 16:22:31 -0500

    Add 3 system view controllers: share, in-app Safari, Camera. Camera only presents 2 actions and prints when user selects them, does not actually handle taking a picture or picking a picutre from the Photos app yet.

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image
+            else {return}
+        
+        //applicationActivities that represents any custom services that your app might support
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil        )
+        
+        //On an iPad, a UIActivityViewController will be presented inside of a popover, and all popovers emanate from a particular view. Popovers are best presented from the button that triggered the presentation; in this case, sender. This line of code will have no effect on smaller iOS devices.
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+        
+    }
 
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+        //URL(string:) initializer returns an optional, so you'll need to unwrap it
+        if let url = URL(string: "http://www.seekershub.org") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+
+    }
+    
+    @IBAction func camerButtonTapped(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil,
+                                                preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in
+            //The alert action has a property called handler, which is where you'll write the code that will be executed if the user selects that option
+            print("User selected Camera action")
+        })
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+            print("User selected Photo Library action")
+        })
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.popoverPresentationController?.sourceView =
+        sender
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
